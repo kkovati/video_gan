9f3cf85e2265c97b2bbcec39495db238961b17c1
Attila Kovács <64501179+kkovati@users.noreply.github.com>
2021-11-15 15:33:18 +0100

    Continue implementation

--- main.py
+++ main.py
@@ -16,12 +16,51 @@ class SquareDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         # https://stackoverflow.com/questions/10031580/how-to-write-simple-geometric-shapes-into-numpy-arrays
         x, y = (self.points[idx] * 100).astype(int)
-        img = np.zeros((128, 128), dtype=int)
-        img[y:y + 20, x:x + 20] = 255
+        img = np.zeros((128, 128), dtype=float)
+        img[y:y + 20, x:x + 20] = 1
         return img
 
 
 class Vae(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        latent_size = 100
+
+        self.enc_conv_0 = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1)
+
+        self.enc_lin_0 = torch.nn.Linear(1 * 126 * 126, int(1 * 126 * 126 / 20))
+        self.enc_lin_1 = torch.nn.Linear(int(1 * 126 * 126 / 20), latent_size)
+
+        self.dec_lin_0 = torch.nn.Linear(latent_size, int(1 * 126 * 126 / 20))
+        self.dec_lin_1 = torch.nn.Linear(int(1 * 126 * 126 / 20), 1 * 126 * 126)
+
+        self.dec_conv_0 = torch.nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=1)
+
+    def forward(self, x):
+        x = self.enc_conv_0(x)
+        x = torch.tanh(x)
+
+        x = torch.flatten(x, start_dim=1)
+        x = self.enc_lin_0(x)
+        x = torch.tanh(x)
+        x = self.enc_lin_1(x)
+        x = torch.tanh(x)
+        l = x
+
+        x = self.dec_lin_0(x)
+        x = torch.tanh(x)
+        x = self.dec_lin_1(x)
+        x = torch.tanh(x)
+
+        x = torch.reshape(x, (x.shape[0], 1, 126, 126))
+
+        x = self.dec_conv_0(x)
+        x = torch.sigmoid(x)
+
+        return x, l
+
+
+"""class VaeV2(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.enc_conv_0 = torch.nn.Conv2d(in_channels=1, out_channels=8, kernel_size=4, stride=2)
@@ -30,9 +69,9 @@ class Vae(torch.nn.Module):
         self.enc_conv_3 = torch.nn.Conv2d(in_channels=8, out_channels=8, kernel_size=4, stride=2)
         self.enc_conv_4 = torch.nn.Conv2d(in_channels=8, out_channels=8, kernel_size=6, stride=1)
 
-        self.enc_lin_0 = torch.nn.Linear(8, 2)
+        self.enc_lin_0 = torch.nn.Linear(8, 10)
 
-        self.dec_lin_0 = torch.nn.Linear(2, 8)
+        self.dec_lin_0 = torch.nn.Linear(10, 8)
 
         self.dec_conv_0 = torch.nn.ConvTranspose2d(in_channels=8, out_channels=8, kernel_size=6, stride=1)
         self.dec_conv_1 = torch.nn.ConvTranspose2d(in_channels=8, out_channels=8, kernel_size=4, stride=2)
@@ -87,33 +126,63 @@ class Vae(torch.nn.Module):
 
         # h_enc = self.encoder(state)
         # z = self._sample_latent(h_enc)
-        return x, l
+        return x, l"""
+
+
+def latent_mean_loss(latent_batch):
+    return torch.mean(torch.abs(torch.mean(latent_batch, dim=0)))
+
+
+def latent_var_loss(latent_batch):
+    return torch.mean(torch.abs(torch.ones(latent_batch.shape[1]) - torch.var(latent_batch, dim=0)))
 
 
 if __name__ == '__main__':
-    square_dataset = SquareDataset(100)
+    square_dataset = SquareDataset(5000)
 
-    train_dataloader = DataLoader(square_dataset, batch_size=256, shuffle=True)
+    train_dataloader = DataLoader(square_dataset, batch_size=32, shuffle=True)
     # test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
 
     vae = Vae()
 
-    criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(vae.parameters(), lr=0.0001)
+    reconstruction_loss = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(vae.parameters(), lr=0.01)
 
-    n_epoch = 10
-    for epoch in range(n_epoch):
-        for img_batch in train_dataloader:
+    n_epoch = 100
+    for i_epoch in range(n_epoch):
+        avg_lml, avg_lvl, avg_rl = 0, 0, 0
+        for i_batch, img_batch in enumerate(train_dataloader):
             img_batch = torch.unsqueeze(img_batch, dim=1).float()
             optimizer.zero_grad()
-            y_hat_batch, latent_batch = vae(img_batch)
-            a = 1
+            recon_img_batch, latent_batch = vae(img_batch)
 
-            # ll = latent_loss(vae.z_mean, vae.z_sigma)
-            # loss = criterion(dec, inputs) + ll
-            # loss.backward()
-            # optimizer.step()
-            # l = loss.data[0]
+            lml = latent_mean_loss(latent_batch)
+            lvl = latent_var_loss(latent_batch)
+            rl = reconstruction_loss(recon_img_batch, img_batch)
+
+            # net_loss = lml + lvl + rl
+            net_loss = rl
+
+            net_loss.backward()
+            optimizer.step()
+
+            avg_lml += lml
+            avg_lvl += lvl
+            avg_rl += rl
+
+            if i_batch == len(train_dataloader) - 1:
+                f, axarr = plt.subplots(1, 2)
+                axarr[0].imshow(np.squeeze(img_batch[0].detach().numpy()), vmin=0, vmax=1)
+                axarr[1].imshow(np.squeeze(recon_img_batch[0].detach().numpy()), vmin=0, vmax=1)
+                # plt.show()
+                plt.savefig(f"epoch_{i_epoch}.png")
+
+        avg_lml /= len(train_dataloader)
+        avg_lvl /= len(train_dataloader)
+        avg_rl /= len(train_dataloader)
+
+        print(f"Epoch {i_epoch + 1}/{n_epoch}")
+        print(f"avg_lml = {avg_lml:.6f}, avg_lvl =  {avg_lvl:.6f}, avg_rl = {avg_rl:.6f}")
 
     # https://towardsdatascience.com/variational-autoencoder-demystified-with-pytorch-implementation-3a06bee395ed
     # https://github.com/ethanluoyc/pytorch-vae/blob/master/vae.py
